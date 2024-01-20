b10effb896803abd9c650e0101fb01875631622f
Faelixx <sasha.taylor7@gmail.com>
2024-01-20 01:21:43 -0700

    Fix: Prevent sql code injection using parameterization

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,5 +1,17 @@
 const { Pool } = require('pg');
-const args = process.argv;
+const cohortName = process.argv[2] || 5;
+const limit = process.argv[3];
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+SELECT students.id AS student_id, 
+students.name AS name,
+cohorts.name AS cohort
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
 
 
 const pool = new Pool ({
@@ -9,15 +21,7 @@ const pool = new Pool ({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id AS student_id, 
-students.name AS name,
-cohorts.name AS cohort
-FROM students
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${args[2]}%'
-LIMIT ${parseInt(args[3])};
-`)
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort.`)
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,18 @@
 const { Pool } = require('pg');
-const args = process.argv;
+const cohortName = process.argv[2];
+const values = [cohortName];
+
+const queryString = `
+SELECT DISTINCT teachers.name AS teacher,
+cohorts.name AS cohort
+FROM assistance_requests
+JOIN teachers ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+GROUP BY teacher, cohorts.name
+ORDER BY teachers.name;
+`
 
 
 const pool = new Pool ({
@@ -9,17 +22,7 @@ const pool = new Pool ({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name AS teacher,
-cohorts.name AS cohort
-FROM assistance_requests
-JOIN teachers ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${args[2]}'
-GROUP BY teacher, cohorts.name
-ORDER BY teachers.name;
-`)
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`)
